6b1864d2002dc5a9e7edced81899fbb9051e2249
Ivaylo Mutafchiev <69365693+imutafchiev94@users.noreply.github.com>
2023-08-13 01:25:48 +0300

    Initialize the server

---
+++ server/src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express with Typescript');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
